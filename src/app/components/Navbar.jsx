1601152bc8cbb86cf00a818f1a9be26ba7ad8857
SAMIR DHORAN <samirdhoran2000@gmail.com>
2023-12-22 22:18:25 +0530

    added navbar

---
+++ src/app/components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import Link from 'next/link';
+import './navbar.css';
+
+const Navbar = () => {
+  return (
+    <div className="nav">
+  <input type="checkbox" id="nav-check"/>
+  <div className="nav-header">
+    <div className="nav-title">
+     Rental Bike
+    </div>
+  </div>
+  <div className="nav-btn">
+    <label for="nav-check">
+      <span></span>
+      <span></span>
+      <span></span>
+    </label>
+  </div>
+  
+          <div className="nav-links">
+              <Link href={"#Home"}>Home</Link>
+              <Link href={"https://shreebikerental.com/pricing/"}>Shree Rental Bikes</Link>
+    <a href="//github.io/jo_geek" target="_blank">Github</a>
+    <a href="http://stackoverflow.com/users/4084003/" target="_blank">Stackoverflow</a>
+    <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">LinkedIn</a>
+    <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
+    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a>
+  </div>
+</div>
+  )
+}
+
+export default Navbar
