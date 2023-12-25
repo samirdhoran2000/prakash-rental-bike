373dc74cda6ca7ca94ab64b27d64c85bdeb743ab
SAMIR DHORAN <samirdhoran2000@gmail.com>
2023-12-25 11:28:53 +0530

    navbar color change and css

--- src/app/components/Footer.jsx
+++ src/app/components/Footer.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import Link from 'next/link';
-import "./footer.css";
+// import "./footer.css";
 
 const Footer = () => {
   return (
--- src/app/components/Navbar.jsx
+++ src/app/components/Navbar.jsx
@@ -5,32 +5,38 @@ import './navbar.css';
 const Navbar = () => {
   return (
     <div className="nav">
-  <input type="checkbox" id="nav-check"/>
-  <div className="nav-header">
-    <div className="nav-title">
-     Rental Bike
-    </div>
-  </div>
-  <div className="nav-btn">
-    <label for="nav-check">
-      <span></span>
-      <span></span>
-      <span></span>
-    </label>
-  </div>
-  
-          <div className="nav-links">
-              <Link href={"/#Home"}>Home</Link>
-              <Link href={"https://shreebikerental.com/pricing/"}>Shree Rental Bikes</Link>
-    <a href="//github.io/jo_geek" target="_blank">Github</a>
+      <input type="checkbox" id="nav-check"/>
+      <div className="nav-header">
+        <div className="nav-title">Rental Bike</div>
+      </div>
+      <div className="nav-btn">
+        <label for="nav-check">
+          <span></span>
+          <span></span>
+          <span></span>
+        </label>
+      </div>
+
+      <div className="nav-links">
+        <Link href={"/#home"}>Home</Link>
+        <Link href={"/#services"}>Services</Link>
+        <Link href={"https://shreebikerental.in/"}>Bikes</Link>
+        <Link href={"/#scooter"}> Scooter</Link>
+        <Link href={"/#about"}>About Us</Link>
+        <Link href={"/#contact"}>Contact Us</Link>
+        <Link href={"https://shreebikerental.com/pricing/"}>
+          Shree Rental Bikes
+        </Link>
+        <Link href={"/#footer"}>Footer</Link>
+
+        {/* <a href="//github.io/jo_geek" target="_blank">Github</a>
     <a href="http://stackoverflow.com/users/4084003/" target="_blank">Stackoverflow</a>
     <a href="https://in.linkedin.com/in/jonesvinothjoseph" target="_blank">LinkedIn</a>
     <a href="https://codepen.io/jo_Geek/" target="_blank">Codepen</a>
-    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a>
-              <Link href={"/#footer"}>Footer</Link>
-  </div>
-</div>
-  )
-}
+    <a href="https://jsfiddle.net/user/jo_Geek/" target="_blank">JsFiddle</a> */}
+      </div>
+    </div>
+  );
+};
 
-export default Navbar
+export default Navbar;
